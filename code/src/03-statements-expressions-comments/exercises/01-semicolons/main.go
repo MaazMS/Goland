e5494f9a0bc3472d68ebeedb353059f892273f35
MaazMS <maazshaikh7020@gmail.com>
2021-04-07 23:18:36 +0530

    semicolons

---
+++ code/src/03-statements-expressions-comments/exercises/01-semicolons/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Shy Semicolons
+//
+//  1. Try to type your statements by separating them using
+//     semicolons
+//
+//  2. Observe how Go fixes them for you
+//
+// ---------------------------------------------------------
+
+func main(){
+	fmt.Println("first statement");fmt.Println("second statement")
+}
