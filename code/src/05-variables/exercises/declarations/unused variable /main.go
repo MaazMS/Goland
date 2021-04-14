5f2531d43d9a0645814d65b7cc9edef4b268cfa1
MaazMS <maazshaikh7020@gmail.com>
2021-04-14 14:12:37 +0530

    unused variable

---
+++ code/src/05-variables/exercises/declarations/unused variable /main.go
@@ -0,0 +1,11 @@
+package main
+
+// there's no warning for package-level vars
+var name string
+func main() {
+	// unused variable error
+	//var speed int
+
+	// if you use it, the error will be gone
+	// fmt.Println(speed)
+}
