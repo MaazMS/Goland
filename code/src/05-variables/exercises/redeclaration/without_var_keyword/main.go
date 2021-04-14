79d9e1efc2ab52da28ea161572c4725fa3785307
MaazMS <maazshaikh7020@gmail.com>
2021-04-14 14:15:16 +0530

    redeclaration without_var_keyword

---
+++ code/src/05-variables/exercises/redeclaration/without_var_keyword/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	name := "Maaz"  // initial value is Maaz
+	name = "Shaikh" // upadate the name variable value is to shaikh
+
+	fmt.Println(name )
+}
