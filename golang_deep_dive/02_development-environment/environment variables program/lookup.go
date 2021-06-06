54d4c2473c1ae6941250152f01d9be2583f8e875
MaazMS <maazshaikh7020@gmail.com>
2021-06-06 11:15:31 +0530

    os.lockup function

--- golang_deep_dive/02_development-environment/environment variables program/lookup.go
+++ golang_deep_dive/02_development-environment/environment variables program/lookup.go
@@ -9,6 +9,11 @@ func main() {
 
 	getEnv := func(key string) {
 		val, ok := os.LookupEnv(key)
+
+		// if value is not set ok variable value is false if value is return it is true.
+		fmt.Println(ok)
+		// if value is false val is empty and if value is not empty it have value
+		fmt.Println(val)
 		if !ok {
 			fmt.Printf("%s is not set \n", key)
 		} else {
