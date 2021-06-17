8a6140c6a8508d8ef976608fbbbc6c0e1e9b3d4c
MaazMS <maazshaikh7020@gmail.com>
2021-06-17 08:37:36 +0530

    nested_struct_to_json_marshal

---
+++ golang_deep_dive/10_Application/nested_struct_to_json_marshal/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Book struct {
+	Title  string     `json:"title"`
+	Author AuthorInfo `json:"author"`
+	test   []struct {
+		One string `json:"one"`
+		Two string `json:"two"`
+	}
+}
+
+type AuthorInfo struct {
+	Name      string `json:"name"`
+	Age       int    `json:"age"`
+	Developer bool   `json:"is_developer"`
+}
+
+func main() {
+
+	authordetails := AuthorInfo{Name: "Abcd",
+		Age:       25,
+		Developer: true,
+	}
+	book := Book{Title: "Golang",
+		Author: authordetails,
+	}
+
+	//byteArray, err := json.Marshal(book)
+	// formatmat json string using josn.Marshalindent
+	byteArray, err := json.MarshalIndent(book, "", "  ")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(byteArray))
+}
