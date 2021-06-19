772d6a5783ff16c1f843c37b0dd81f5797efcd88
MaazMS <maazshaikh7020@gmail.com>
2021-06-19 11:00:14 +0530

    Ninja Level 9

--- golang_deep_dive/Ninja_Level_9/exercise_1.go
+++ golang_deep_dive/Ninja_Level_9/exercise_1.go
@@ -1,1 +1,34 @@
-package Ninja_Level_9
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+/*
+● in addition to the main goroutine, launch two additional goroutines
+	○ each additional goroutine should print something out
+● use waitgroups to make sure each goroutine finishes before your program exists
+*/
+
+var wg sync.WaitGroup
+
+func main() {
+
+	fmt.Println("function main")
+	//runtime.Gosched()
+	wg.Add(2)
+	go foo()
+	go bar()
+	wg.Wait()
+
+}
+
+func foo() {
+	fmt.Println("function foo")
+	wg.Done()
+}
+func bar() {
+	fmt.Println("function bar")
+	wg.Done()
+}
