85759bee1a6b67540d05e3338bf7ec738bc7e772
MaazMS <maazshaikh7020@gmail.com>
2021-06-19 09:12:52 +0530

    Ninja Level 8

--- golang_deep_dive/Ninja_Level_8/exercise_4.go
+++ golang_deep_dive/Ninja_Level_8/exercise_4.go
@@ -1,1 +1,95 @@
 package main
+
+/*Starting with this code line 52, sort the []user by
+● age
+● last
+Also sort each []string “Sayings” for each user
+● print everything in a way that is pleasant
+*/
+import (
+	"fmt"
+	"sort"
+)
+
+type user1 struct {
+	First   string
+	Last    string
+	Age     int
+	Sayings []string
+}
+
+type ByAge []user1
+
+func (a ByAge) Len() int           { return len(a) }
+func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
+
+type ByLast []user1
+
+func (l ByLast) Len() int           { return len(l) }
+func (l ByLast) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
+func (l ByLast) Less(i, j int) bool { return l[i].Last < l[j].Last }
+
+func main() {
+	u1 := user1{
+		First: "James",
+		Last:  "Bond",
+		Age:   32,
+		Sayings: []string{
+			"Shaken, not stirred",
+			"Youth is no guarantee of innovation",
+			"In his majesty's royal service",
+		},
+	}
+
+	u2 := user1{
+		First: "Miss",
+		Last:  "Moneypenny",
+		Age:   27,
+		Sayings: []string{
+			"James, it is soo good to see you",
+			"Would you like me to take care of that for you, James?",
+			"I would really prefer to be a secret agent myself.",
+		},
+	}
+
+	u3 := user1{
+		First: "M",
+		Last:  "Hmmmm",
+		Age:   54,
+		Sayings: []string{
+			"Oh, James. You didn't.",
+			"Dear God, what has James done now?",
+			"Can someone please tell me where James Bond is?",
+		},
+	}
+
+	users := []user1{u1, u2, u3}
+	for _, u := range users {
+		fmt.Println(u.First, u.Last, u.Age)
+		sort.Strings(u.Sayings)
+		for _, v := range u.Sayings {
+			fmt.Println("\t", v)
+		}
+	}
+
+	fmt.Println("-------")
+
+	sort.Sort(ByAge(users))
+	for _, u := range users {
+		fmt.Println(u.First, u.Last, u.Age)
+		for _, v := range u.Sayings {
+			fmt.Println("\t", v)
+		}
+	}
+
+	fmt.Println("-------")
+
+	sort.Sort(ByLast(users))
+	for _, u := range users {
+		fmt.Println(u.First, u.Last, u.Age)
+		for _, v := range u.Sayings {
+			fmt.Println("\t", v)
+		}
+	}
+}
