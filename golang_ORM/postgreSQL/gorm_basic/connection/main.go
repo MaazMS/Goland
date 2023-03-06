93e16cd826867d5264fd5fc20cfa53d7380fa652
MaazMS <maazshaikh7020@gmail.com>
2023-03-06 14:24:02 +0530

    ORM with postgresql

---
+++ golang_ORM/postgreSQL/gorm_basic/connection/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "p"
+	dbname   = "users"
+)
+
+func main() {
+
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db, err := gorm.Open(postgres.Open(psqlconn), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect to the Database")
+	}
+
+	fmt.Println("Connection to the Database created")
+	fmt.Println(db)
+
+}
